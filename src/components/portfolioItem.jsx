2f1e60a3a719060c491f99a3baf2c8fcad3267ff
Rowan Holtzclaw <OmegaSystem8@gmail.com>
2023-04-30 20:09:42 -0600

    Changed asset path, beautified portfolio item frames

--- src/components/portfolioItem.jsx
+++ src/components/portfolioItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function portfolioItem({title, imgUrl, stack, link}){
     return(
-        <div>
+        <div className="border-2 border-stone-900 rounded-md overflow-hidden">
             <img 
                 src={imgUrl}
                 alt='portfolio'
@@ -11,9 +11,9 @@ function portfolioItem({title, imgUrl, stack, link}){
             />
             <div className='w-full p-4'>
                 <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold"> {title} </h3>
-                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs">
+                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
                     {stack.map(item => (
-                        <span>
+                        <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-md">
                             {item} 
                         </span>
                     ))}
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
     title: 'Developer Portfolio',
-    imgUrl: 'src/assets/dev.png',
+    imgUrl: '/assets/dev.png',
     stack: ['JavaScript','React','Tailwind','Vite'],
     link: 'https://cedarsaga.github.io'
    },
    {
     title: 'Developer Portfolio',
-    imgUrl: 'src/assets/dev.png',
+    imgUrl: '/assets/dev.png',
     stack: ['JavaScript','React','Tailwind','Vite'],
     link: 'https://cedarsaga.github.io'
    },
    {
     title: 'Developer Portfolio',
-    imgUrl: 'src/assets/dev.png',
+    imgUrl: '/assets/dev.png',
     stack: ['JavaScript','React','Tailwind','Vite'],
     link: 'https://cedarsaga.github.io'
    }
