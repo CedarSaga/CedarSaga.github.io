3569ffcda6a5021a0d6e812701f45dd6b6262897
Rowan Holtzclaw <OmegaSystem8@gmail.com>
2023-04-30 19:30:15 -0600

    Removed extraneous folder#

--- src/components/portfolioItem.jsx
+++ src/components/portfolioItem.jsx
@@ -10,8 +10,8 @@ function portfolioItem({title, imgUrl, stack, link}){
                 className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
             <div className='w-full p-4'>
-                <h3> {title} </h3>
-                <p>
+                <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold"> {title} </h3>
+                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs">
                     {stack.map(item => (
                         <span>
                             {item} 
