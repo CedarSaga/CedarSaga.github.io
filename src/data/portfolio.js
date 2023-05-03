ed5ba8fc1fc198e8121b57a5554e093d551de3f1
Rowan Holtzclaw <OmegaSystem8@gmail.com>
2023-05-03 15:55:28 -0600

    Updated Assets.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -8,19 +8,19 @@ export default [
    {
     title: 'Boids',
     imgUrl: '/assets/Boids.png',
-    stack: ['Python', 'O.O.P'],
+    stack: ['Python', 'O.O.P', 'Kinematics'],
     link: 'https://cedarsaga.github.io'
    },
    {
     title: 'Tarot Reader',
-    imgUrl: '/assets/Tarot.jpg',
-    stack: ['Java', 'C++'],
+    imgUrl: '/assets/Tarot.png',
+    stack: ['Python (v3),', 'Java (v2)', 'C++ (v1)'],
     link: 'https://cedarsaga.github.io'
     },
     {
      title: 'Twitter Bot',
      imgUrl: '/assets/TwitterBot.png',
-     stack: ['Python', 'APIs'],
+     stack: ['Python', 'APIs', 'Web Integration'],
      link: 'https://cedarsaga.github.io'
     }
 ]
